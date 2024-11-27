64a70350006c70ae673351626b7b18ddf7d2f3a4
Xiangwei Tang <v-xianta@microsoft.com>
2024-11-27 10:32:34 +0800

    Update

---
+++ SentenceCountPR/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SentenceCountPR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputpath;
+            inputpath = Console.ReadLine();
+            SentenceCount sentenceCount = new SentenceCount(inputpath);
+        }
+    }
+
+    internal class SentenceCount
+    {
+        public SentenceCount(string path)
+        {
+            int i = -1;
+            try
+            {
+                i = countFolder(path);
+            }
+            catch (Exception e)
+            { }
+
+            if (i == -1)
+            {
+                try
+                {
+                    i = countFile(path);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            if (i != -1)
+                Console.WriteLine(i);
+        }
+
+        int countFolder(string s)
+        {
+            int i = 0;
+            foreach (string s1 in Directory.GetFiles(s))
+            {
+                foreach (string s2 in File.ReadAllLines(s1))
+                    i++;
+            }
+
+            return i;
+        }
+
+        int countFile(string s)
+        {
+            int i = 0;
+            foreach (string s1 in File.ReadAllLines(s))
+                i++;
+            return i;
+        }
+    }
+}
---
+++ SentenceCountPR/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SentenceCountPR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SentenceCountPR")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84386f86-7b68-4b41-bc50-54d756de8e00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
